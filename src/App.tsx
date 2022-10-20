1b75cd9327b6f1c7f1ed508a0362f9a62d181c61
Oleg <akimba@gmail.com>
2022-10-20 18:52:55 +0300

    feat: Отрисовывает список продуктов

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { useFetch } from './hooks/useFetch';
 import { Product } from './types/product';
+import ProductItem from './components/product-item/product-item';
 import { ApiRoute } from './const';
 
 function App() {
 
-  const {value, loadingStatus} = useFetch<Product[]>(ApiRoute.Product, []);
+  const {value, loadingStatus, error} = useFetch<Product[]>(ApiRoute.Product, []);
 
   if (loadingStatus) {
-    return <h1>Loading... Please wait</h1>
+    return <h1 className='text-center'>Loading... Please wait</h1>
+  }
+  if (error) {
+    return <h1 className='text-center text-red-500'>Something strange is going here...</h1>
   }
   return (
-    <div>
-      Hello world!
-      {value.join()}
-    </div>
+    <section className='mx-auto p-4 w-[960px]'>
+      <h2 className="mb-6  text-center text-3xl font-bold">Доступные товары</h2>
+      <ul className='mx-auto w-[600px]'>
+        {value.map((data) => <ProductItem productData={data} key={data.id} />)}
+      </ul>
+    </section>
   );
 }
 
---
+++ src/components/product-item/product-item.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Product } from '../../types/product';
+
+type ProductProps = {
+  productData: Product;
+};
+
+function ProductItem({productData}: ProductProps): JSX.Element {
+  const {title, description, price, rating, image} = productData;
+  const [isDescriptionOpen, setDescriptionStatus] = useState<boolean>(false);
+
+  const buttonCustomStyle = isDescriptionOpen ? 'bg-blue-500' : 'bg-yellow-500';
+  const buttonStyles = ['w-56 h-10 rounded', buttonCustomStyle];
+
+  const handleButtonClick = () => {
+    setDescriptionStatus(prev => !prev);
+  };
+
+  return (
+    <li className='flex flex-col mb-4 p-4 border-2 rounded border-slate-400'>
+      <h3 className='mb-6 text-center text-2xl font-bold'>{title}</h3>
+      <img src={image} className="block mx-auto mb-4 w-40 h-40" alt="picture about image" />
+      <div className='flex justify-between items-center mb-4'>
+        <p className='font-bold'>{`Рейтинг товара: ${rating.rate}`}</p>
+        <div className='flex items-center gap-x-4'>
+          <p className='font-bold'>{`Цена: ${price}$`}</p>
+          <button
+            onClick={handleButtonClick}
+            className={buttonStyles.join(' ')}
+          >
+            {isDescriptionOpen ? 'Скрыть описание' : 'Показать описание'}
+          </button>
+        </div>
+      </div>
+      {isDescriptionOpen && <p>{description}</p>}
+    </li>
+  );
+}
+
+export default ProductItem;
