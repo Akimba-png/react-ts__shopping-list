96de817642e885bec2dc25a01e8d23f90f683b7e
Oleg <akimba@gmail.com>
2022-10-25 14:24:14 +0300

    ref: Переносит логику загрузки данных из App в Catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { useFetch } from './hooks/useFetch';
-import { Product } from './types/product';
 import Catalog from './pages/catalog/catalog';
-import MainLoader from './components/loaders/main-loader/main-loader';
-import ErrorNotifier from './components/error-notifier/error-notifier';
-import { ApiContext } from './context/api-context';
-import { ApiRoute, Message } from './const';
 
 function App() {
-  const {value, addValue, loadingStatus, error} = useFetch<Product>(ApiRoute.Product, []);
   return (
-    <ApiContext.Provider value={{addValue}}>
-      <div className='container mx-auto'>
-        {loadingStatus && <MainLoader /> }
-        {error && <ErrorNotifier message={Message.LoadingFail} additionalStyle='text-xl' />}
-        {(!loadingStatus && !error) && <Catalog catalogData={value} />}
-    </div>
-    </ApiContext.Provider>
+    <Catalog />
   );
 }
 
--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -1,36 +1,46 @@
 import { useContext } from 'react';
+import { useFetch } from '../../hooks/useFetch';
+import { ApiContext } from '../../context/api-context';
+import { ModalContext } from './../../context/modal-context';
 import { Product } from './../../types/product';
+import MainLoader from '../../components/loaders/main-loader/main-loader';
+import ErrorNotifier from '../../components/error-notifier/error-notifier';
 import ProductItem from '../../components/product-item/product-item';
 import Modal from '../../components/modal/modal';
 import CatalogForm from './../../components/catalog-form/catalog-form';
-import { ModalContext } from './../../context/modal-context';
+import { ApiRoute, Message } from '../../const';
 
-type CatalogProps = {
-  catalogData: Product[],
-}
-
-function Catalog({catalogData}: CatalogProps): JSX.Element {
+function Catalog(): JSX.Element {
+  const {value: catalogData, addValue, loadingStatus, error} = useFetch<Product>(ApiRoute.Product, []);
   const { isModalOpen, handlerModalOpen, handlerModalClose } = useContext(ModalContext);
+
   return (
-    <section className='mx-auto p-4 w-[960px]'>
-      <h1 className="mb-6  text-center text-3xl font-bold">Доступные товары</h1>
-      <ul className='mx-auto w-[600px]'>
-        {catalogData.map((data, i) => <ProductItem productData={data} key={`${data.id}-${i}`} />)}
-      </ul>
-      {isModalOpen &&
-        <Modal
-          onClose={handlerModalClose}
-        >
-          <CatalogForm />
-        </Modal>
-        }
-      {!isModalOpen && <button
-        onClick={handlerModalOpen}
-        className='fixed right-[350px] bottom-10 px-4 py-2 w-40  bg-red-500 rounded'
-      >
-        Добавить в каталог
-      </button>}
-    </section>
+    <ApiContext.Provider value={{addValue}}>
+      {loadingStatus && <MainLoader /> }
+      {error && <ErrorNotifier message={Message.LoadingFail} additionalStyle='text-xl' />}
+      {(!loadingStatus && !error) && (
+        <section className='constainer mx-auto p-4 w-[960px]'>
+          <h1 className="mb-6 text-center text-3xl font-bold">Доступные товары</h1>
+          <ul className='mx-auto w-[600px]'>
+            {catalogData.map((data, i) => <ProductItem productData={data} key={`${data.id}-${i}`} />)}
+          </ul>
+          {isModalOpen &&
+            <Modal
+              onClose={handlerModalClose}
+            >
+              <CatalogForm />
+            </Modal>
+          }
+          {!isModalOpen && <button
+            onClick={handlerModalOpen}
+            className='fixed right-[350px] bottom-10 px-4 py-2 w-40  bg-red-500 rounded'
+            >
+              Добавить в каталог
+            </button>
+          }
+        </section>
+      )}
+    </ApiContext.Provider>
   );
 }
 
