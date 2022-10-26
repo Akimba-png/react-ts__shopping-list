04ddec534570923308c625c2cc2b90cdb1785c69
Oleg <akimba@gmail.com>
2022-10-26 10:49:12 +0300

    Добавляет вёрстку страницы Info

--- src/pages/info/info.tsx
+++ src/pages/info/info.tsx
@@ -1,6 +1,26 @@
+const TOTAL_AMOUNT = 12000;
+
 function Info(): JSX.Element {
+  const data = new Array(TOTAL_AMOUNT).fill(null).map((_, i) => `Товар ${i + 1}`)
   return (
-    <h1>This is info page</h1>
+    <section className="container flex flex-col items-center mx-auto pt-4 w-[960px]">
+      <h1 className="mb-1 font-bold text-3xl">Полный перечень товаров</h1>
+      <p className="mb-4">{`Всего доступно товаров: ${data.length}`}</p>
+      <form className="mb-4">
+        <label className="font-bold text-md">
+          Введите номер товара:
+          <input
+            type="text"
+            name="filter-input"
+            placeholder="12345"
+            className="ml-4 px-2 py-1 border border border-2 border-gray-500 rounded"
+            />
+        </label>
+      </form>
+      <ul>
+        {data.map(lot => (<li key={lot}>{lot}</li>))}
+      </ul>
+    </section>
   );
 }
 
